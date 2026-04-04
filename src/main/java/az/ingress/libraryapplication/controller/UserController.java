e18da762f768ed4386a539959b8bdeb713146036
Sanan Huseynli <senanhuseynlee@gmail.com>
2026-04-04 14:22:04 +0400

    Senan first commit

--- src/main/java/az/ingress/libraryapplication/controller/UserController.java
+++ src/main/java/az/ingress/libraryapplication/controller/UserController.java
@@ -1,4 +1,12 @@
 package az.ingress.libraryapplication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class UserController {
+
+    @GetMapping
+    public ModelAndView showUser(){
+        return new ModelAndView("User");
+    }
 }
